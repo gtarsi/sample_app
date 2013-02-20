be70f6137cb41ebe51b894411ea2a6659280ceec
Grant Tarsi <gtarsi87@gmail.com>
2013-02-19 22:56:11 -0500

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,16 +1,24 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
     	visit '/static_pages/home'
       	page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the right title" do
-    	visit '/static_pages/home'
-    	page.should have_selector('title',
-    		:text => "Ruby on Rails Tutorial Sample App | Home")
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
@@ -18,12 +26,12 @@ describe "StaticPages" do
   	it "should have the content 'Help'" do
   		visit '/static_pages/help'
   		page.should have_selector('h1', :text => 'Help')
-	end
+    end
 
-	it "should have the right title" do
+	   it "should have the right title" do
     	visit '/static_pages/help'
     	page.should have_selector('title',
-    		:text => "Ruby on Rails Tutorial Sample App | Help")
+    		:text => "#{base_title} | Help")
     end
   end
 
@@ -36,7 +44,20 @@ describe "StaticPages" do
   	it "should have the right title" do
     	visit '/static_pages/about'
     	page.should have_selector('title',
-    		:text => "Ruby on Rails Tutorial Sample App | About Us")
+    		:text => "#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+  
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', 
+        :text => "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
